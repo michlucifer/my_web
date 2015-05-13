5b4dc2c83b8f702cb90a586857f0be736d28fd12
michlucifer <juehou4102@gmail.com>
2015-05-14 00:31:47 +0800

    login page

--- login.php
+++ login.php
@@ -4,35 +4,50 @@
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
 	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css" media="screen" type="text/css" />
+<style type="text/css">
+table, td, tr {
+ border:1px solid #ffffff;
+ border-collapse:collapse;
+}
+</style>
 </head>
 	<body>
-		<form action="login_process.php" method="POST" class="navbar-form navbar-left">
-			<div class="form-group">
-				<table class="table">
-					<tr>
-						<td>
-							<label>
-								UserID:
-							</label>
-						</td>
-						<td>
-							<input type="text" class="form-control" name="userid" placeholder="Type in your userid" style="width:300px;height:50px;border-radius:0;"/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>
-								Password:
-							</label>
-						</td>
-						<td>
-							<input type="password" class="form-control" name="pass" placeholder="Type in your password" style="width:300px;height:50px;border-radius:0;"/>
-						</td>
-					</tr>
-				</table>
+		<div class="container-fluid" style="background:#ffffff;">
+			<div class="login-page">
+			<form action="login_process.php" method="POST" class="navbar-form">
+				<div class="form-group">
+					<table class="login-input  table">
+						<tr>
+							<td>
+								<label>
+									UserID:
+								</label>
+							</td>
+							<td>
+								<input type="text" class="form-control" name="userid" placeholder="Type in your userid" style="width:300px;height:50px;border-radius:0;"/>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>
+									Password:
+								</label>
+							</td>
+							<td>
+								<input type="password" class="form-control" name="pass" placeholder="Type in your password" style="width:300px;height:50px;border-radius:0;"/>
+							</td>
+						</tr>
+						<tr>
+							<td><button type="submit" class="btn btn-default">LOGIN</button></td>
+						</tr>
+					</table>
+				</div>
 			</div>
-					<input type="submit" value="LOGIN" style="background-color:rgba(255,255,255,0.7); border:0px; height:50px;"/>
-				</form>
-		<a href="new_user.php">Signup Here!</a>
+			</form>
+			<div class="register-notice">
+				<span>Not Registerd Yet? &nbsp;&nbsp;</span><a href="new_user.php">Signup Here!</a>
+			</div>
+			
+		</div>
 	</body>
 </html>
